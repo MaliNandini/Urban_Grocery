c9f75f8b9103f816b58d2195a0f8b542ed766576
unknown <san2deep11@gmail.com>
2023-03-10 15:54:42 +0530

    Nabar Home Page Filter Method

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './Component/Navbar/Navbar';
+import { Route,Routes} from 'react-router-dom';
+import { ProductDetails } from './Component/Product-Details/ProductDetails';
+import { Card } from './Component/Card/Card';
+import Home from './Component/Home';
+
 
 function App() {
   return (
     <div className="App">
-      
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+
+     <Routes>
+        <Route path="/" element={<Home/>} />
+        {/* <Route path="/product-details/:id" element={<ProductDetails/>} /> */}
+        <Route path="/product-details/1" element={<ProductDetails/>} />
+      </Routes>
     </div>
   );
 }
